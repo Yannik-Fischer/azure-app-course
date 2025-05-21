e00bd271b1b57373e9086e45385851b337c38cc1
Yannik-Fischer <yannik.fischer@adesso.de>
2025-05-21 12:00:26 +0200

    Create console app to retrieve queue messages

---
+++ Console.QueueConsumer.Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Text;
+
+var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+var queueClient = new QueueClient(connectionString, "attendee-emails");
+
+if (await queueClient.ExistsAsync())
+{
+    QueueProperties properties = await queueClient.GetPropertiesAsync();
+
+    for (var i = 0; i < properties.ApproximateMessagesCount; i++)
+    {
+        var message = await RetrieveNextMessage();
+        Console.WriteLine($"Received: {message}");
+    }
+}
+
+async Task<string> RetrieveNextMessage()
+{
+    QueueMessage[] retrievedMessages = await queueClient.ReceiveMessagesAsync(1);
+    var data = Convert.FromBase64String(retrievedMessages[0].Body.ToString());
+    var message = Encoding.UTF8.GetString(data);
+
+    await queueClient.DeleteMessageAsync(retrievedMessages[0].MessageId, retrievedMessages[0].PopReceipt);
+
+    return message;
+}
