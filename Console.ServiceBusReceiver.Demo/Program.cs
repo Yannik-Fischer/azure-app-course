9f64030bfac7630f61b52921196b341b67b4fb25
Yannik-Fischer <yannik.fischer@adesso.de>
2025-05-21 16:27:34 +0200

    Add message bus sender and receiver

---
+++ Console.ServiceBusReceiver.Demo/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Messaging.ServiceBus;
+
+const string QueueName = "az-course-queue-1";
+
+var ServiceBusConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGEBus_CONNECTION_STRING");
+
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    var body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine(body);
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processErrorEventArgs)
+{
+    Console.WriteLine(processErrorEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(ServiceBusConnectionString);
+processor = client.CreateProcessor(QueueName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Press any key to stop the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ Console.ServiceBusSender.Demo/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Messaging.ServiceBus;
+
+const string QueueName = "az-course-queue-1";
+const int MaxMessageCount = 5;
+
+var ServiceBusConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGEBus_CONNECTION_STRING");
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(ServiceBusConnectionString);
+sender = client.CreateSender(QueueName);
+
+try
+{
+    using (var batch = await sender.CreateMessageBatchAsync())
+    {
+        for (var i = 1; i <= MaxMessageCount; i++)
+        {
+            if (!batch.TryAddMessage(new ServiceBusMessage($"This is message {i}")))
+            {
+                Console.WriteLine($"Message {i} was not added to the batch");
+            }
+        }
+
+        await sender.SendMessagesAsync(batch);
+        Console.WriteLine("Messages sent");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
