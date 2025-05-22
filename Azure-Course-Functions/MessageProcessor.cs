548323145b4b993e2624f59cea999f7625c26d18
Yannik-Fischer <yannik.fischer@adesso.de>
2025-05-22 12:03:36 +0200

    Added Azure functions with vscode

---
+++ Azure-Course-Functions/MessageProcessor.cs
@@ -0,0 +1,22 @@
+using System;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzCourse.Function;
+
+public class MessageProcessor
+{
+    private readonly ILogger<MessageProcessor> _logger;
+
+    public MessageProcessor(ILogger<MessageProcessor> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(MessageProcessor))]
+    public void Run([QueueTrigger("message-queue", Connection = "AzureWebJobsStorage")] QueueMessage message)
+    {
+        _logger.LogInformation("C# Queue trigger function processed: {messageText}", message.MessageText);
+    }
+}
---
+++ Azure-Course-Functions/MessageReceiver.cs
@@ -0,0 +1,29 @@
+using Azure.Storage.Queues.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzCourse.Function;
+
+public class MessageReceiver
+{
+    private readonly ILogger<MessageReceiver> _logger;
+
+    public MessageReceiver(ILogger<MessageReceiver> logger)
+    {
+        _logger = logger;
+    }
+
+    [QueueOutput("message-queue")]
+    [Function("MessageReceiver")]
+    public async Task<string> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        // msg.Add(requestBody);
+
+        return requestBody;
+    }
+}
